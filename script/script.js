c4cd260fb330542e99875c62cc0554d187bcb1a9
Raser006 <100070448+Raser006@users.noreply.github.com>
2023-01-09 21:44:08 +0400

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,56 @@
+$('.burger').on('click', function() {
+    $('.padshi').toggleClass('padshi_active');
+  })
+
+
+$('.vip-button').on('click', function() {
+    $('.vip').toggleClass('vip_active');
+})
+$('.krest').on('click', function() {
+    $('.vip').toggleClass('vip_active');
+})
+
+$('.zon-button').on('click', function() {
+    $('.zon').toggleClass('zon_active');
+})
+$('.krest1').on('click', function() {
+    $('.zon').toggleClass('zon_active');
+})
+
+$('.vr-button').on('click', function() {
+    $('.vr').toggleClass('vr_active');
+})
+$('.krest2').on('click', function() {
+    $('.vr').toggleClass('vr_active');
+})
+
+$('.kal-button').on('click', function() {
+    $('.kal').toggleClass('kal_active');
+})
+$('.krest3').on('click', function() {
+    $('.kal').toggleClass('kal_active');
+})
+
+$('.zabron').on('click', function() {
+    $('.bron').toggleClass('bron_active');
+})
+$('.krest4').on('click', function() {
+    $('.bron').toggleClass('bron_active');
+
+})
+
+
+
+$(window).load(function(){
+    $(".preload").fadeOut("slow");
+})
+
+
+
+
+
+
+
+
+
+
---
+++ script/send.php
@@ -0,0 +1,47 @@
+<?php
+$name = 'name' 
+$email = 'e-mail'
+$msg = 'komm'
+
+$name = trim(urldecode(htmlspecialchars($name)));
+$email = trim(urldecode(htmlspecialchars($email)));
+$msg = trim(urldecode(htmlspecialchars($msg)));
+
+if(mail("evgeniaakperova@gmail.com", "Новый запрос на бронирование антикафе", 
+"<h1>На вашем сайте была оставлена заявка<h1>
+<br>от: <b>".$name."</b>
+<br>e-mail: <b>".$email."</b>
+<br>пользователь оставил комментарий ".$msg."
+<br>Поздравляю с новой заявкой!",
+"From: test.ru\r\n". "Content-type: text/html\r\n"))
+{
+    echo '{"status": "ok"}';
+}else{
+    echo '{"status": "error"}';
+}
+
+if($('#name').val() != '' && $('#email').val() !='') {
+
+}else{
+    alert("Заполните обязательные поля")
+}
+
+fetch('send.php', {
+    method: 'POST',
+    headers:{
+        'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: $("#send_form").serialize(),
+    }).then((response) => response.json())
+
+.then((data) => {
+    if (data.status === 'ok') {
+        $("#send_form").addClass("send_success");
+        setTimeout(() => $("send_form").removeClass("send_success"), 4000);
+    }
+    if (data.status === 'error') {
+        $("#send_form").addClass("send_fail");
+        setTimeout(() => $("send_form").removeClass("send_fail "), 4000);
+    }
+}
+?>
